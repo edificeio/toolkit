7ad237bddb6f80fd964c85205da535cb0b296f66
Camille Wintz <camille.wintz@web-education.net>
2016-11-23 10:26:40 +0100

    removed index.ts (index.js is maintained manually)

--- index.ts
+++
@@ -1,6 +0,0 @@
-export * from './src/minicast';
-export * from './src/eventer';
-export * from './src/selection';
-export * from './src/http.interface';
-export * from './src/crud';
-export * from './src/http';
